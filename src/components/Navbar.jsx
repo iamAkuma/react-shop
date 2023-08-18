4aac95fd3d232c1cf20415d411249ae50c8aede4
Saral Bajracharya <rmanbajra@gmail.com>
2023-08-18 19:28:52 +0545

    Modal added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,33 @@
 import {Button, Container, Navbar, Modal} from 'react-bootstrap'
-
+import { useState } from 'react'
 
 const NavbarComponent = () => {
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false)
+  const handleShow = () => setShow(true)
   return (
-    <div>
-      <Navbar expand='sm'>
+    <>
+    <Container>
+    <Navbar expand='sm'>
         <Navbar.Brand href= '/'>Tero Bau ko Pasal</Navbar.Brand> 
         <Navbar.Toggle/>
         <Navbar.Collapse className='justify-content-end'>
-            <Button>
+            <Button onClick={handleShow}>
                 Cart 0 Items
             </Button>
         </Navbar.Collapse>
       </Navbar>
-    </div>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Shopping Cart</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <h1>This is the modal body.</h1>
+        </Modal.Body>
+      </Modal>  
+    </Container>
+      
+    </>
   )
 }
 
