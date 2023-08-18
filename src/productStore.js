f918fb321d9fa05652b03434a1ff71d596a67e67
Saral Bajracharya <rmanbajra@gmail.com>
2023-08-18 19:14:56 +0545

    Making our own data

---
+++ src/productStore.js
@@ -0,0 +1,30 @@
+const productsArray = [
+    {
+        id: '1',
+        name: 'Espresso',
+        price: 150
+    },
+    {
+        id: '2',
+        name: 'Milk Tea',
+        price: 100
+    },
+    {
+        id: '3',
+        name: 'Latte',
+        price: 150
+    },
+
+]
+
+function getProductData(id) {
+    let productData = productsArray.find(product => product.id === id)
+
+    if(productData === undefined) {
+        console.log("Product data does not exist for ID: " +id)
+        return undefined;
+    }
+    return productData
+}
+
+export {productsArray}
